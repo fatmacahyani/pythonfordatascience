e618c57b15aeb2f77f73c8b75ebf4beb6288f954
Fatma Cahyani <85814520+fatmacahyani@users.noreply.github.com>
2022-01-12 12:29:41 +0700

    Create writing_generator_to_load_data(3).py

---
+++ writing_generator_to_load_data(3).py
@@ -0,0 +1,25 @@
+'''
+To process the data, We will create another DataFrame composed of only the rows from a specific country. Then zip together two of the columns from the new DataFrame.
+Finally, We will create a list of tuples from the zip object, where each tuple is composed of a value from each of the two columns mentioned.
+'''
+
+# Initialize reader object: urb_pop_reader
+urb_pop_reader = pd.read_csv('ind_pop_data.csv', chunksize = 1000)
+
+# Get the first DataFrame chunk: df_urb_pop
+df_urb_pop = next(urb_pop_reader)
+
+# Check out the head of the DataFrame
+print(df_urb_pop.head())
+
+# Check out specific country: df_pop_ceb
+df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode'] == 'CEB']
+
+# Zip DataFrame columns of interest: pops
+pops = zip('Total Population', 'Urban population (% of total)')
+
+# Turn zip object into list: pops_list
+pops_list = list(pops)
+
+# Print pops_list
+print(pops_list)
