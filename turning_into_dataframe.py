4a23b9cd13b0ebf47a656251c6f378090b4feba1
Fatma Cahyani <85814520+fatmacahyani@users.noreply.github.com>
2022-01-12 11:43:47 +0700

    Create turning_into_dataframe.py

---
+++ turning_into_dataframe.py
@@ -0,0 +1,15 @@
+'''
+After zipped lists together, we will convert the list of dictionaries into a pandas DataFrame with DataFrame() function from the pandas package.
+'''
+
+# Import the pandas package
+import pandas as pd
+
+# Turn list of lists into list of dicts: list_of_dicts
+list_of_dicts = [lists2dict(feature_names, sublist) for sublist in row_lists]
+
+# Turn list of dicts into a DataFrame: df
+df = pd.DataFrame(list_of_dicts)
+
+# Print the head of the DataFrame
+print(df.head())
